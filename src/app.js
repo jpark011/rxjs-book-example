da23220841fc0d432c766210fd9c99d46725d302
jay(박현재) <jay.now@kakaocorp.com>
2021-10-04 00:45:40 +0900

    지도에 경로 표시

--- src/app.js
+++ src/app.js
@@ -8,4 +8,10 @@ const search = new AutoComplete(document.querySelector('.autocomplete'));
 const sidebar = new Sidebar(document.querySelector('.stations'));
 const map = new Map(document.querySelector('.map'), search$);
 
-render$.subscribe((v) => console.log(v));
+render$.subscribe((stations) => {
+  if (stations.length) {
+    map.drawPath(stations);
+  } else {
+    map.deletePath();
+  }
+});
